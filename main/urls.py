b97315dc14c51b54a7a1cf8bb52fc55fc8f8308f
Toghrul <baghirli.togrul@gmail.com>
2024-07-10 19:06:43 +0400

    django in progress

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+
+import main.views
 from . import views
+from learning import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
+    path('', main.views.index),
+    path('about', main.views.about),
+    path('hoho', views.hoho),
+    path('img', views.page),
+
 ]
